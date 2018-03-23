a6e68f14ce578e0b4240e3672fb1efd0717e8839
yoshikiito <yoshikiito.el@gmail.com>
2018-03-24 02:03:02 +0900

    app-genデバッグ中。。。

--- app-gen.js
+++ app-gen.js
@@ -17,7 +17,7 @@ var app = new Vue({
   data: data,
   methods: {
     showText: function() {
-      var random = Math.random() * questions.lentgh;
+      var random = Math.random() * data.questions.lentgh;
       this.qText = this.questions[random];
     }
   }
